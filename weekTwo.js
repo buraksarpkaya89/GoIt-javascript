70b46b44b1cd05aed00232fe9e0fe0870177fca8
buraksarpkaya89 <burak.sarpkaya@smartopt.com.tr>
2025-01-07 21:38:03 +0300

    weekTwoLesson2

--- weekTwo.js
+++ weekTwo.js
@@ -4,7 +4,7 @@
 // bir veya birden fazla değişkeni bir veya birden fazla değer ile kontrol etme
 
 let numara = 50
-let numara1= 44
+let numara1 = 44
 
 if (numara < 50) {
     console.log("Numara 50 den küçük");
@@ -65,20 +65,20 @@ switch (numara) {
 
 // if Örneği
 
-function IndirimFiyatHesapla(urunAdedi, urunFiyatı){
-    
-    if(urunAdedi <= 0){
+function IndirimFiyatHesapla(urunAdedi, urunFiyatı) {
+
+    if (urunAdedi <= 0) {
         return "Bir ürün almadınız"
-    }else if(urunAdedi < 5){
+    } else if (urunAdedi < 5) {
         let indirimliFiyat = urunFiyatı * 0.9
         return indirimliFiyat * urunAdedi
-    }else if(urunAdedi <= 10){
+    } else if (urunAdedi <= 10) {
         let indirimliFiyat = urunFiyatı * 0.8
         return indirimliFiyat * urunAdedi
-    }else if(urunAdedi >= 50){
+    } else if (urunAdedi >= 50) {
         let indirimliFiyat = urunFiyatı * 0.5
         return indirimliFiyat * urunAdedi
-    }else{
+    } else {
         let indirimliFiyat = urunFiyatı * 0.7
         return indirimliFiyat * urunAdedi
     }
@@ -90,63 +90,118 @@ function IndirimFiyatHesapla(urunAdedi, urunFiyatı){
 let urunFiyatı = 100
 let urunAdedi = 8
 let toplamFiyat = IndirimFiyatHesapla(urunAdedi, urunFiyatı)
-console.log(`Aldığınız ürün kampanya kapsamında ${toplamFiyat} ile satışa hazırdır`);
+// console.log(`Aldığınız ürün kampanya kapsamında ${toplamFiyat} ile satışa hazırdır`);
 
 // Mantıksal "VE” - Logical AND (&&),  VEYA OPERATORU||
 
 
 let valuee = "istanbul"
- 
 
-if(valuee === "istanbul" && valuee.length === 8 ){
-    console.log("ve dogru");
-}else if(valuee === "istanbul" || urunAdedi === 9 )
-    console.log("veya doğrusu");
-else{
-    console.log("yanlış");
-}
+
+// if(valuee === "istanbul" && valuee.length === 8 ){
+//     console.log("ve dogru");
+// }else if(valuee === "istanbul" || urunAdedi === 9 )
+//     console.log("veya doğrusu");
+// else{
+//     console.log("yanlış");
+// }
 
 // Mantıksal “DEĞIL” - Logical NOT (!)
 
 const bloklu = false;
 const chatYapabilirmi = !bloklu; // !false -> true
 
-if(chatYapabilirmi) {
-	console.log("Can type in chat!")
-} else {
-	console.log("Blocked from typing in chat!")
-}
+// if(chatYapabilirmi) {
+// 	console.log("Can type in chat!")
+// } else {
+// 	console.log("Blocked from typing in chat!")
+// }
 
 
 
 // switch örneği
 
-function harfNotuAlma(not){
-    switch(true){
-        case (not >= 90 && not <= 100) :
-            console.log("Harf Notu A");
-            break;
-        case (not >=80 && not < 90):
-            console.log("Harf notu B");
-            break;
-        case ( 70 <= not < 80) :
-            console.log("Harf notu C");
-            console.log(`Ortalama notunuz ${totalPoint}`);
-            break;
-        case (not>= 60 && not <70):
-            console.log("Harf notu D");
-            break;
-        
-        default :
-            console.log("Sınıfta kaldınız");
-        
-    }
-}
+// function harfNotuAlma(not){
+//     switch(true){
+//         case (not >= 90 && not <= 100) :
+//             console.log("Harf Notu A");
+//             break;
+//         case (not >=80 && not < 90):
+//             console.log("Harf notu B");
+//             break;
+//         case ( 70 <= not < 80) :
+//             console.log("Harf notu C");
+//             console.log(`Ortalama notunuz ${totalPoint}`);
+//             break;
+//         case (not>= 60 && not <70):
+//             console.log("Harf notu D");
+//             break;
+
+//         default :
+//             console.log("Sınıfta kaldınız");
+
+//     }
+// }
 let arasinav = 30
 let notDeğeri = 75
 
 let totalPoint = (arasinav + notDeğeri) / 2
-harfNotuAlma(notDeğeri)
+// harfNotuAlma(notDeğeri)
 
+// LESSON TWO
+
+
+
+function checkForName(fullName, firstName) {
+    return fullName.toLowerCase().includes(firstName.toUpperCase());
+}
+
+// console.log(checkForName("Burak Sarpkaya", "burak"));
+
+// const myString = "Merhaba GOIT"
+// const index = myString.indexOf("r")
+
+// console.log(index); //2
+
+// if(index !== -1){
+//     const result = myString.slice(index + 1)
+//     console.log(result.toUpperCase());
+// }else{
+//     console.log("ifade içerisinde yoktur");
+// }
+
+let count = 10;
+
+// while (count < 5) {
+//   count += 1;
+//   console.log(`Count: ${count}`);
+
+// }
+
+
+
+// do {
+// 	count += 1;
+//     console.log(`Count: ${count}`);
+
+// } while (count < 5);
+
+// for (let i = 0; i <= 20; i += 5) {
+//     console.log(i);
+// }
+// for(let index = 100; index >= 0 ; index -= 10){
+//     console.log(index);
+// }
+
+// for (let i = 0; i < 10; i += 1) {
+//     console.log(i);
+
+//     if (i === 5) {
+//         console.log('Met the number 5, interrupt the execution of the cycle');
+//         break;
+//     }
+// }
+
+// console.log('Log after cycle');
 
 
